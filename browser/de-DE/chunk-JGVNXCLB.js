d448e6a09b01c278deae6b5c38ad0bbab262a037
Slou A <slowautomobile@protonmail.com>
2025-05-03 19:10:10 +0200

    Deploy dist update

--- browser/de-DE/chunk-JGVNXCLB.js
+++
@@ -1,2 +0,0 @@
-import{T as a}from"./chunk-2HC3UJHZ.js";import"./chunk-2KRI6ZBU.js";import"./chunk-EZNRQUAM.js";import"./chunk-IQUW5QUP.js";import"./chunk-FYGCA5T3.js";import"./chunk-QABDRPPI.js";import"./chunk-CSA5XVMK.js";import"./chunk-MOGN5K2H.js";export{a as BasicProgramsTableComponent};
-/**i18n:e9421f27e91e2550a5a0bd03ad694673877e5a8020798e378092da15c61db7f3*/
